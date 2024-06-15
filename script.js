4a2703601fd799eff5c7d2bdbe5a0d4b520af6ec
Connor <119803598+Connor-Partington@users.noreply.github.com>
2024-06-15 17:49:12 +0100

    added default books function and function to create book cards

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const addFormDialog = document.querySelector(".add-book-dialog");
 const bookForm = document.querySelector(".add-book-form");
 const closeDialogButton = addFormDialog.querySelector(".close-dialog-button");
 const addBookDialogButton = addFormDialog.querySelector(".add-book-dialog-button");
-const bookTable = document.getElementById("demo");
+const bookTable = document.getElementById("book-container");
 
 addBookButton.addEventListener("click", () => {
     addFormDialog.showModal();
@@ -42,16 +42,30 @@ function addBookToLibrary(title, author, pages, isRead) {
     const newBook = new Book(title, author, pages, isRead);
     myLibrary.push(newBook);
     displayBooksFromLibrary();
-    console.log(myLibrary);
+}
+
+function addDefaultBooksToLibrary(title, author, pages, isRead) {
+    const book1 = new Book("book1", "author1", 12, "read");
+    const book2 = new Book("book2", "author2", 12, "notread");
+    const book3 = new Book("book3", "author3", 12, "notread");
+    const book4 = new Book("book4", "author4", 12, "notread");
+    myLibrary.push(book1, book2, book3, book4);
+    displayBooksFromLibrary();
 }
 
 function displayBooksFromLibrary() {
-    let books = "";
+    bookTable.innerHTML = "";
     for (const [index, x] of myLibrary.entries()) {
-        console.log(index, x);
-        books += index + " " + x.title + " " + x.author +  " " + x.pages +  " " + x.isRead +  "<br>";
+        const bookCard = document.createElement('div');
+		bookCard.classList.add('book-card');
+        bookCard.dataset.indexValue = index;
+		bookCard.innerHTML = 
+			`<div>${x.title}</div>
+			<div>${x.author}</div>
+			<div>${x.pages}</div>
+            <div>${x.isRead}</div>`;
+        bookTable.append(bookCard);
     }
-    bookTable.innerHTML = books;
 }
 
 function checkForBooks() {
@@ -60,4 +74,5 @@ function checkForBooks() {
     }
 }
 
+addDefaultBooksToLibrary();
 checkForBooks()
